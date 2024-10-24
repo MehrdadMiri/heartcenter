f744f166c084e6df6c63b235d291501e13e23781
Mehrdad Miri <mehrdad.miri@snapp.cab>
2024-10-24 14:18:51 +0330

    add index page

--- heartcenter/urls.py
+++ heartcenter/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views  # Import the views module
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),  # Add this line for the main page
     path('diseases/', include('diseases.urls', namespace='diseases')),
     path('medications/', include('medications.urls', namespace='medications')),
     path('supplements/', include('supplements.urls', namespace='supplements')),
