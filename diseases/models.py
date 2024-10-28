c5d577ccfefc628ca4da1c309bf22506c882ef82
Mehrdad Miri <mehrdad.miri@snapp.cab>
2024-10-28 16:15:35 +0330

    simplify models

--- diseases/models.py
+++ diseases/models.py
@@ -4,9 +4,7 @@ from heartcenter.settings import LANGUAGE_CHOICES
 
 class Disease(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
-    symptoms = models.TextField()
-    treatments = models.TextField()
+    text = models.TextField()
     lang = models.CharField(max_length=2, choices=LANGUAGE_CHOICES)
 
     def __str__(self):
--- medications/models.py
+++ medications/models.py
@@ -4,9 +4,7 @@ from heartcenter.settings import LANGUAGE_CHOICES
 
 class Medication(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
-    uses = models.TextField()
-    side_effects = models.TextField()
+    text = models.TextField()
     lang = models.CharField(max_length=2, choices=LANGUAGE_CHOICES)
 
     def __str__(self):
